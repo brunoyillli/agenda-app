a548f97a99105ceff297b09468798741c93775fe
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-25 15:31:19 -0300

    criando componente e servico e fomulario dos contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button'
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import {MatInputModule} from '@angular/material/input';
+
+import { ContatoComponent } from './contato/contato.component'
+import { ContatoService } from './contato.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ContatoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
