44c7b8c85dce9190a313b4779158ff78d9f4d541
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-31 14:39:24 -0300

    melhorias visuais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
+
 import { HttpClientModule } from '@angular/common/http'
 import { ContatoComponent } from './contato/contato.component'
 import { ContatoService } from './contato.service';
@@ -24,6 +29,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule
