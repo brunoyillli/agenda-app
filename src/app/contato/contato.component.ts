567bb6435845c4601f6308465d195d1bb1dbd6e2
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-01 21:18:18 -0300

    melhoria na atualizacao da lista

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -48,7 +48,8 @@ export class ContatoComponent implements OnInit {
     const formValues = this.formulario.value;
     const contato: Contato = new Contato(formValues.nome, formValues.email);
     this.service.save(contato).subscribe(resposta => {
-      this.contatos.push(resposta);
+      let lista: Contato[] = [...this.contatos, resposta];
+      this.contatos = lista;
     })
   }
 }
