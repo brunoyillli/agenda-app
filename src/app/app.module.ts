b99ed487011075e4ef26234e65889a789de354c8
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-02 23:17:00 -0300

    alguns ajustes e adicionado snack bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 import { HttpClientModule } from '@angular/common/http';
 import { ContatoComponent } from './contato/contato.component';
@@ -39,6 +40,7 @@ import { ContatoDetalheComponent } from './contato-detalhe/contato-detalhe.compo
     MatCardModule,
     MatDialogModule,
     MatPaginatorModule,
+    MatSnackBarModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -5,6 +5,7 @@ import { Contato } from './contato';
 import { MatDialog } from "@angular/material/dialog"
 import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component';
 import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -25,7 +26,8 @@ export class ContatoComponent implements OnInit {
   constructor(
     private service: ContatoService,
     private fb: FormBuilder,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -59,8 +61,11 @@ export class ContatoComponent implements OnInit {
     const formValues = this.formulario.value;
     const contato: Contato = new Contato(formValues.nome, formValues.email);
     this.service.save(contato).subscribe(resposta => {
-      let lista: Contato[] = [...this.contatos, resposta];
-      this.contatos = lista;
+      this.listarContatos();
+      this.snackBar.open('O Contato foi adicionado!', 'Sucesso!', {
+        duration: 2000
+      });
+      this.formulario.reset();
     })
   }
 
